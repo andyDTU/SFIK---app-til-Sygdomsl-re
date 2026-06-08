600ce5a0911214b0739427f3737f010bb573fb90
andyDTU <s251271@dtu.dk>
2026-06-08 19:18:39 +0200

    Initial Commit

---
+++ SFIK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SFIK
+//
+//  Created by Anders Juul Madsen on 08/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SFIK/SFIKApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFIKApp.swift
+//  SFIK
+//
+//  Created by Anders Juul Madsen on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SFIKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
